803e160fccd85f164efe376d95bd386276a893e4
yhwit30 <yhwit30@gmail.com>
2025-04-09 21:21:39 +0900

    4단계, WiseSaying 클래스 생성, List에  명언 저장

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -14,6 +16,8 @@ public class App {
         System.out.println("== motivation 앱 실행 == ");
 
         int lastId = 1;
+        int listCount = 0;
+        List<WiseSaying> wiseSayingList = new ArrayList<>();
 
         while(true){
             System.out.print("명령어) ");
@@ -25,16 +29,25 @@ public class App {
                 System.out.print("인물 : ");
                 String person = sc.nextLine();
                 System.out.printf("%d번 명언이 등록되었습니다.\n", lastId);
+
+                WiseSaying temp = new WiseSaying(lastId, content, person);
+                wiseSayingList.add(temp);
+
                 lastId++;
+                listCount++;
+            }
+            else if(cmd.equals("list")){
+                System.out.printf("등록된 명언 수 : %d\n", listCount);
             }
 
-            if (cmd.equals("exit")){
+            else if (cmd.equals("exit")){
                 System.out.println("== motivation 앱 종료 ==");
                 break;
             }
+            else {
+                System.out.println("존재하지 않는 명령어입니다.");
+            }
+
         }
-
     }
-
-
 }
