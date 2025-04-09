b512ddab994e608a22a75b400eaa2484fb6b048e
yhwit30 <yhwit30@gmail.com>
2025-04-09 20:34:46 +0900

    2단계, 명언 입력받기

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,13 +13,22 @@ public class App {
     public void run(){
         System.out.println("== motivation 앱 실행 == ");
 
-        // 혹시 모를 공백 제거 : trim()
-        System.out.print("명령어) ");
-        String cmd = sc.nextLine().trim();
+        while(true){
+            System.out.print("명령어) ");
+            String cmd = sc.nextLine().trim();
 
+            if(cmd.equals("add")){
+                System.out.print("명언 : ");
+                String content = sc.nextLine();
+                System.out.print("인물 : ");
+                String person = sc.nextLine();
+                System.out.println("1번 명언이 등록되었습니다.");
+            }
 
-        if (cmd.equals("exit")){
-            System.out.println("== motivation 앱 종료 ==");
+            if (cmd.equals("exit")){
+                System.out.println("== motivation 앱 종료 ==");
+                break;
+            }
         }
 
     }
