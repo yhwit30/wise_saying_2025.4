7251dd9e9377e76b6f81a7117d1634d5a0012c88
yhwit30 <yhwit30@gmail.com>
2025-04-10 21:44:53 +0900

    Rq 클래스 도입, 명령어 분석 기능 요청

--- src/main/java/org/project/App.java
+++ src/main/java/org/project/App.java
@@ -3,10 +3,6 @@ package org.project;
 import org.system.controller.SystemController;
 import org.wiseSaying.controller.WiseSayingController;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 public class App {
 
     App() {
@@ -21,21 +17,10 @@ public class App {
             System.out.print("명령어) ");
             String cmd = Container.getScanner().nextLine().trim();
             if (cmd.startsWith("delete")) {
-                // parsing start
-                String[] cmdBits = cmd.split("\\?", 2);
-                String actionCode = cmdBits[0];
-                Map<String, String> params = new HashMap<>();
+                Rq rq = new Rq(cmd);
 
-                String[] paramBits = cmdBits[1].split("=", 2);
-
-                String key = paramBits[0];
-                String value = paramBits[1];
-                params.put(key, value);
-                System.out.println(Arrays.toString(cmdBits));
-                System.out.println("actioncode : " + actionCode);
-                System.out.println("key : " + key);
-                System.out.println("value : " + value);
-                // parsing end
+                System.out.println(rq.getActionCode());
+                System.out.println(rq.getParams());
 
                 wiseSayingController.remove();
 
